64ba81ee896d548420d6ceff0e6b1e71bb77b363
placidohoff <67982091+placidohoff@users.noreply.github.com>
2021-11-03 01:35:22 -0400

    Form Validation Working

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -26,7 +26,7 @@ export default class Contact extends React.Component {
     }
 
     changeForm(e) {
-        this.setState({...this.state, name: e.target.value, errors:{name:false, email: this.state.errors.email, comment: this.state.errors.comment} })
+        this.setState({ ...this.state, name: e.target.value, errors: { name: false, email: this.state.errors.email, comment: this.state.errors.comment } })
     }
 
     isError(val) {
@@ -48,20 +48,32 @@ export default class Contact extends React.Component {
     }
 
     handleSubmit(e) {
-        if (this.state.name === '') {
-            this.setState({ ...this.state, errors: { name: true, email: this.state.errors.email, comment: this.state.errors.comment } })
-        }
-        else if (this.state.email === '') {
-            this.setState({ ...this.state, errors: { name: this.state.errors.name, email: true, comment: this.state.errors.comment } })
-
-        }
-        // if(this.isError(this.state.email)){
-        //     this.setState({})
+        // if (this.state.name === '') {
+        //     // this.setState({ ...this.state, errors: { name: true, email: this.state.errors.email, comment: this.state.errors.comment } })
+        //     this.setState({ 
+        //         ...this.state, 
+        //         errors: { name: true, email: this.state.errors.email, comment: this.state.errors.comment } })
+        //     // this.setState({...this.state.errors, name:true})
         // }
-        else if (this.state.comment === '') {
-            this.setState({ ...this.state, errors: { name: this.state.errors.name, email: this.state.errors.email, comment: true } })
+        // if (this.state.email === '') {
+        //     // this.setState({ ...this.state, errors: { name: this.state.errors.name, email: true, comment: this.state.errors.comment } })
+        //     this.setState({...this.state.errors, email:true})
+        // }
+        // // if(this.isError(this.state.email)){
+        // //     this.setState({})
+        // // }
+        // if (this.state.comment === '') {
+        //     // this.setState({ ...this.state, errors: { name: this.state.errors.name, email: this.state.errors.email, comment: true } })
 
-        }
+        // }
+
+        let isNameEmpty = this.state.name === '' ? true : false
+        let isEmailEmpty = this.state.email === '' ? true : false
+        let isCommentEmpty = this.state.comment === '' ? true : false
+
+        this.setState({ ...this.state, errors: { name: isNameEmpty, email: isEmailEmpty, comment: isCommentEmpty } })
+
+
 
         console.log(this.state)
         e.preventDefault()
@@ -111,7 +123,7 @@ export default class Contact extends React.Component {
                                     className="form-control"
                                     type="text"
                                     value={this.state.email}
-                                    onChange={e => this.setState({ email: e.target.value })}
+                                    onChange={e => this.setState({ email: e.target.value, errors: {name: this.state.errors.name, email: false, comment: this.state.errors.comment} })}
                                     onBlur={e => this.setState({ emailTouched: true })}
                                 />
                             </div>
@@ -121,6 +133,10 @@ export default class Contact extends React.Component {
                             <FormError
                                 message="Email format is invalid"
                             />
+                        }
+                        {
+                            this.state.errors.email &&
+                            <FormError message="empty" />
                         }
                     </div>
 
@@ -135,8 +151,19 @@ export default class Contact extends React.Component {
                         <div className="row form-group">
                             <div className="col">
                                 {/* <input type="text" value={this.state.email} onChange={e => this.setState({email: e.target.value})} /> */}
-                                <textarea className="form-control" rows="12" cols="24" />
+                                <textarea 
+                                    className="form-control" 
+                                    rows="12" 
+                                    cols="24" 
+                                    value={this.state.comment}
+                                    onChange={e => {this.setState({comment: e.target.value, errors: {name: this.state.errors.name, email: this.state.errors.email, comment: false}})}}
+                                />
+
                             </div>
+                            {
+                                this.state.errors.comment &&
+                                <FormError message="empty" />
+                            }
                         </div>
                     </div>
                     <br />
